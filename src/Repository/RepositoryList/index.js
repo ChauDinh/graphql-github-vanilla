2c1df65bb7eaa3d46dfb093b4734792ca3722cd0
Chau Dinh <chaudvb95uc@gmail.com>
2019-12-13 16:02:24 +0700

    styled repository list component

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -3,11 +3,14 @@ import React from "react";
 import RepositoryItem from "../RepositoryItem";
 import "../style.css";
 
-const RepositoryList = ({ repositories }) =>
-  repositories.edges.map(({ node }) => (
-    <div key={node.id} className="RepositoryItem">
-      <RepositoryItem {...node} />
-    </div>
-  ));
+const RepositoryList = ({ repositories }) => (
+  <div className="RepositoryList">
+    {repositories.edges.map(({ node }) => (
+      <div key={node.id} className="RepositoryItem">
+        <RepositoryItem {...node} />
+      </div>
+    ))}
+  </div>
+);
 
 export default RepositoryList;
