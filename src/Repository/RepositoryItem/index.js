82d8e47037a2ee4c69b94cedf75f57f55eaa0829
Chau Dinh <chaudvb95uc@gmail.com>
2019-12-13 19:46:11 +0700

    graphql mutation for starring repository

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
 
 import Link from "../../Link";
+import Button from "../../Button";
 import "../style.css";
 
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
 const RepositoryItem = ({
+  id,
   name,
   url,
   descriptionHTML,
@@ -20,8 +35,21 @@ const RepositoryItem = ({
         <Link href={url}>{name}</Link>
       </h2>
 
-      <div className="RepositoryItem-title-action">
+      {/* <div className="RepositoryItem-title-action">
         {stargazers.totalCount} Stars
+      </div> */}
+      <div>
+        {!viewerHasStarred ? (
+          <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+            {(addStar, { data, loading, error }) => (
+              <Button className="RepositoryItem-title-action" onClick={addStar}>
+                {stargazers.totalCount} Star
+              </Button>
+            )}
+          </Mutation>
+        ) : (
+          <span></span>
+        )}
       </div>
     </div>
 
